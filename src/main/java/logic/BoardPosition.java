b9208ff346a90fd64230e1bbf2201a533a69c316
Gad Wissberg <gadw17@gmail.com>
2019-05-25 16:48:26 +0300

    Added PawnInterface and BoardPosition.

---
+++ src/main/java/logic/BoardPosition.java
@@ -0,0 +1,18 @@
+package logic;
+
+public class BoardPosition {
+    int row, col;
+
+    public BoardPosition(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+}
