ff3d02198bcb8b588ee0764a1470701076897cba
bar <bar@root.co.il>
2019-05-31 17:29:40 +0300

    JSON Class - save complete (board missing, waiting for some required board getters)

--- src/main/java/logic/Json.java
+++ src/main/java/logic/Json.java
@@ -4,50 +4,84 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
 
+import javax.xml.ws.Response;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.Writer;
 import java.lang.reflect.Array;
 
 public class Json {
-    //private json;
+    private static final String JSON_FILE = "src"+File.separator+"main"+File.separator+"game.json";
 
-    // convert sring to json
+    // Encode game session to Json
     public Gson Encode(Session s) {
         Gson j = new Gson();
-        String jsonOut;
-
-
 
         JsonArray arr = new JsonArray();
 
-        JsonObject obj = new JsonObject();
+        // create objects for later use
+        JsonObject o  = new JsonObject();
+        JsonObject o2 = new JsonObject();
 
         Player p;
 
         // Player 1
         p = s.getPlayer1();
-        obj.addProperty("name", p.getName());
-        obj.addProperty("color", p.getColor().toString());
-        obj.addProperty("score", p.getScore());
-
-        arr.add(obj);
+        o.addProperty("name", p.getName());
+        o.addProperty("color", p.getColor().toString());
+        o.addProperty("score", p.getScore());
+        o2.add("player1", o);
+        arr.add(o2);
 
         // Player 2
         p = s.getPlayer2();
-        obj = new JsonObject();
-        obj.addProperty("name", p.getName());
-        obj.addProperty("color", p.getColor().toString());
-        obj.addProperty("score", p.getScore());
+        o = new JsonObject();
+        o2 = new JsonObject();
+        o.addProperty("name", p.getName());
+        o.addProperty("color", p.getColor().toString());
+        o.addProperty("score", p.getScore());
+        o2.add("player2", o);
+        arr.add(o2);
 
-        arr.add(obj);
+        // current turn
+        o = new JsonObject();
+        o.addProperty("turn", s.getCurrentTurn().getName());
+        arr.add(o);
 
+        // board status
+        o = new JsonObject();
+        JsonArray board = new JsonArray();
+        board.add("soon"); // TODO: write board status function, to deliver: board size and each player positions
+        o.add("board", board);
+        arr.add(o);
 
         System.out.println(j.toJson((arr)));
 
         return j;
     }
 
-    // convert json to array
-    /*public function Decode(String json) {
+    private void save2File(Gson json) {
+        // output json to file
+        try (Writer writer = new FileWriter(JSON_FILE)) {
+            Gson gson = new GsonBuilder().create();
+            gson.toJson(json, writer);
+        } catch(Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
-    }*/
+    // convert json (from file) to array
+    public void Decode() {
+        try {
+            Gson gson = new Gson();
+            JsonReader reader = new JsonReader(new FileReader(JSON_FILE));
+            //Response data = gson.fromJson(reader);
+        } catch(Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
