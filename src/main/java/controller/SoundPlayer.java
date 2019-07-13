bd3c806ff64795d6d45551d74ae8772ca9175a60
Gad Wissberg <gadw17@gmail.com>
2019-07-13 17:21:45 +0300

    Added sounds.

---
+++ src/main/java/controller/SoundPlayer.java
@@ -0,0 +1,26 @@
+package controller;
+
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+
+public class SoundPlayer {
+    private static final String SOUNDS_PATH = "src" + File.separator + "main" + File.separator + "resources" + File.separator;
+
+    public void playSound(Sound sound) {
+        String soundName = sound.name() + ".wav";
+        try {
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(SOUNDS_PATH + soundName.toLowerCase()).getAbsoluteFile());
+            Clip clip = null;
+            clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+            clip.start();
+        } catch (LineUnavailableException | UnsupportedAudioFileException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public enum Sound {CLICK, EAT, MOVE}
+}
