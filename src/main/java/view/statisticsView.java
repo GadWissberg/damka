3a3294a9fcd9c607562fd731d500b902adc7cf6f
c_rkishn <bbb>
2019-07-14 21:45:37 +0300

    added statistics view

---
+++ src/main/java/view/statisticsView.java
@@ -0,0 +1,26 @@
+package view;
+
+import interfaces.ViewListener;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class statisticsView extends JFrame {
+
+    JFrame statFrame = new JFrame();
+
+    public statisticsView(ViewListener viewListener) {
+        super("Games statistics");
+        this.setSize(new Dimension(300, 160));
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        JLabel gamesWonOne = new JLabel("Player 1:  " +viewListener.getGamesWonOne());
+        JLabel gamesWonTwo = new JLabel("Player 2:  " +viewListener.getGamesWonOne());
+        gamesWonOne.setFont (gamesWonOne.getFont().deriveFont(34.0f));
+        gamesWonTwo.setFont (gamesWonTwo.getFont().deriveFont(34.0f));
+        panel.add(gamesWonOne);
+        panel.add(gamesWonTwo);
+        add(panel);
+    }
+
+}
